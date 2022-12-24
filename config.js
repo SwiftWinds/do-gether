5ef1c6c2d51636a52071236b8847612c95270026
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-12-24 11:24:27 -0800

    Add home without styling
    Former-commit-id: 238cd8c125fd5c13b9abae8a67eb37e1390c2c05

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ2FY69u3jR8WMVLCT_TDrkKyqkUE2Y3k",
+  authDomain: "dogether-78b6f.firebaseapp.com",
+  projectId: "dogether-78b6f",
+  storageBucket: "dogether-78b6f.appspot.com",
+  messagingSenderId: "1038829035917",
+  appId: "1:1038829035917:web:05d7bee0c0c5f18a28ddc4",
+  measurementId: "G-VFJ7E1L2CC"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
