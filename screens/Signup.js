0925eb7f68aff79d8a1f6d80ceea774165425f38
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-12-24 18:34:46 -0800

    Open home on click login temporarily

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,40 +1,31 @@
-import {
-  View,
-  Text,
-  ImageBackground,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, ImageBackground, TextInput, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
 
 import InlineBtn from "../components/InlineBtn";
 import AppStyles from "../styles/AppStyles";
 
-export default function Login() {
+export default function Signup() {
   const backgroundImg = require("../assets/background.jpg");
 
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-  const navigation = useNavigation();
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validationMsg, setValidationMsg] = useState("");
 
   return (
     <ImageBackground style={AppStyles.container} source={backgroundImg}>
       <View style={AppStyles.backgroundCover}>
-        <Text style={[AppStyles.lightText, AppStyles.header]}>
-          Log in to Dogether
-        </Text>
+        <Text style={[AppStyles.lightText, AppStyles.header]}>Create a Dogether account</Text>
         <TextInput
           style={[
             AppStyles.textInput,
             AppStyles.lightTextInput,
             AppStyles.lightText,
           ]}
-          placeholder="Email"
+          placeholder="Username"
           placeholderTextColor={"#bebebe"}
-          onChangeText={setEmail}
-          value={email}
+          onChangeText={setUsername}
+          value={username}
         />
         <TextInput
           style={[
@@ -48,19 +39,23 @@ export default function Login() {
           onChangeText={setPassword}
           value={password}
         />
-        <View style={AppStyles.rowContainer}>
-          <InlineBtn>Forgot your password?</InlineBtn>
-        </View>
+        <TextInput
+          style={[
+            AppStyles.textInput,
+            AppStyles.lightTextInput,
+            AppStyles.lightText,
+          ]}
+          placeholder="Confirm Password"
+          placeholderTextColor={"#bebebe"}
+          secureTextEntry={true}
+          onChangeText={setConfirmPassword}
+          value={confirmPassword}
+        />
         <TouchableOpacity style={AppStyles.btn} underlayColor="#fff">
-          <Text
-            style={[AppStyles.lightText, AppStyles.loginBtnText]}
-            onPress={() => navigation.navigate("Home")}
-          >
-            Login
-          </Text>
+          <Text style={[AppStyles.lightText, AppStyles.loginBtnText]}>Login</Text>
         </TouchableOpacity>
         <View style={AppStyles.rowContainer}>
-          <Text style={AppStyles.lightText}>Don't have an account yet? </Text>
+          <Text style={AppStyles.lightText}>Already have an account? </Text>
           <InlineBtn>Sign up</InlineBtn>
         </View>
       </View>
