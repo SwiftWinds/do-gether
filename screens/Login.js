237bf74230b1560025423824913bc28a811f7693
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-12-25 19:18:49 -0800

    Make login and signup actually work

--- screens/Login.js
+++ screens/Login.js
@@ -1,3 +1,7 @@
+import { useNavigation } from "@react-navigation/native";
+import to from "await-to-js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -5,10 +9,9 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React, { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
 
 import InlineBtn from "../components/InlineBtn";
+import { auth } from "../config";
 import AppStyles from "../styles/AppStyles";
 
 export default function Login() {
@@ -17,7 +20,31 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [errorMsg, setErrorMsg] = useState("");
+
   const navigation = useNavigation();
+
+  const validate = () => {
+    if (!email || !password) {
+      setErrorMsg("Please fill in all fields");
+    } else {
+      login();
+    }
+  };
+
+  const login = async () => {
+    // we cannot destructure userCredentials.user here because it will crash if userCredentials is undefined
+    const [error, userCredentials] = await to(
+      signInWithEmailAndPassword(auth, email, password)
+    );
+    if (error) {
+      setErrorMsg(error.message);
+      return;
+    }
+    const { user } = userCredentials;
+    console.log(user);
+    navigation.navigate("Home");
+  };
 
   return (
     <ImageBackground style={AppStyles.container} source={backgroundImg}>
@@ -32,7 +59,7 @@ export default function Login() {
             AppStyles.lightText,
           ]}
           placeholder="Email"
-          placeholderTextColor={"#bebebe"}
+          placeholderTextColor="#bebebe"
           onChangeText={setEmail}
           value={email}
         />
@@ -43,18 +70,23 @@ export default function Login() {
             AppStyles.lightText,
           ]}
           placeholder="Password"
-          placeholderTextColor={"#bebebe"}
-          secureTextEntry={true}
+          placeholderTextColor="#bebebe"
+          secureTextEntry
           onChangeText={setPassword}
           value={password}
         />
         <View style={AppStyles.rowContainer}>
-          <InlineBtn onPress={() => navigation.navigate("ResetPassword")}>Forgot your password?</InlineBtn>
+          <InlineBtn onPress={() => navigation.navigate("ResetPassword")}>
+            Forgot your password?
+          </InlineBtn>
+        </View>
+        <View>
+          <Text style={AppStyles.errorText}>{errorMsg}</Text>
         </View>
         <TouchableOpacity
           style={AppStyles.btn}
           underlayColor="#fff"
-          onPress={() => navigation.navigate("Home")}
+          onPress={validate}
         >
           <Text style={[AppStyles.lightText, AppStyles.loginBtnText]}>
             Log in
