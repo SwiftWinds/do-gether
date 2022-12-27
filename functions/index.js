3055a8038aa27edc980c9321daadea0e0ec3628b
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-12-26 23:40:50 -0800

    Fix firebase functions eslint nits
    Former-commit-id: 68a0f29a7444db0eb7ea8b015995767f9129c38c

--- functions/index.js
+++ functions/index.js
@@ -4,40 +4,36 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const resetStreaksAndDeleteTodos = async () => {
   // get all users
   const users = await db.collection("users").get();
   // loop through all users
-  return Promise.all(users.docs.map(async (user) => {
-    // get user data
-    const userData = user.data();
-    // if user has a partner
-    if (userData.partner) {
-      // if not all todos are verified, reset streak
-      // get todos subcollection ref
-      const todosRef = user.ref.collection("todos");
-      // query todos subcollection for all todos
-      const todos = await todosRef.get();
-      // check if all todos are verified
-      const allTodosVerified = todos.docs.every((todo) => todo.data().verified);
-      // if not all todos are verified, reset streak
-      if (!allTodosVerified) {
-        await user.ref.update({
-          streak: 0,
-        });
-      }
-      // delete all todos
-      return Promise.all(todos.docs.map((todo) => todo.ref.delete()));
-    }
-  }));
+  return Promise.all(
+      users.docs.map(async (user) => {
+      // get user data
+        const userData = user.data();
+        // if user has a partner
+        if (userData.partner) {
+        // if not all todos are verified, reset streak
+        // get todos subcollection ref
+          const todosRef = user.ref.collection("todos");
+          // query todos subcollection for all todos
+          const todos = await todosRef.get();
+          // check if all todos are verified
+          const allTodosVerified = todos.docs.every(
+              (todo) => todo.data().verified,
+          );
+          // if not all todos are verified, reset streak
+          if (!allTodosVerified) {
+            await user.ref.update({
+              streak: 0,
+            });
+          }
+          // delete all todos
+          return Promise.all(todos.docs.map((todo) => todo.ref.delete()));
+        }
+      }),
+  );
 };
 
 const deleteImages = async () => {
@@ -48,8 +44,8 @@ const deleteImages = async () => {
 
 // runs every day at 5:00am LA time
 exports.dailyReset = functions.pubsub
-  .schedule("0 5 * * *")
-  .timeZone("America/Los_Angeles")
-  .onRun(async (context) => {
-    await Promise.all([resetStreaksAndDeleteTodos(), deleteImages()]);
-  });
+    .schedule("0 5 * * *")
+    .timeZone("America/Los_Angeles")
+    .onRun(async (context) => {
+      await Promise.all([resetStreaksAndDeleteTodos(), deleteImages()]);
+    });
