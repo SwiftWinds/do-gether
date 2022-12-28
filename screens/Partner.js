272921aa8edf2b9c54a15d5d0b96165186dd35ea
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-12-27 16:42:12 -0800

    Fix User linking
    Former-commit-id: 8f0329b0874010405be7c479e28e994d71ebfd70
    Former-commit-id: 5833c724ff082c04538cfc2ee0b571ddc4c2b903

--- screens/Partner.js
+++ screens/Partner.js
@@ -1,44 +1,151 @@
+import { Ionicons } from "@expo/vector-icons";
+import to from "await-to-js";
 import * as Clipboard from "expo-clipboard";
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  onSnapshot,
+  doc,
+  collection,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Pressable,
+} from "react-native";
 
-import { auth } from "../config";
+import { auth, db } from "../config";
 
 const WEB_API_KEY = "AIzaSyCJ2FY69u3jR8WMVLCT_TDrkKyqkUE2Y3k";
 
 const copyInvitationUrl = async () => {
-  try {
-    const payload = {
-      dynamicLinkInfo: {
-        domainUriPrefix: "https://dogether.page.link",
-        link: `https://dogether-78b6f.web.app/user/${auth.currentUser.uid}`,
-        androidInfo: {
-          androidPackageName: "gg.dogether.app",
-        },
-        socialMetaTagInfo: {
-          socialTitle: `New invite from ${auth.currentUser.displayName}`,
-          socialDescription: `${auth.currentUser.displayName} is using Dogether to stay accountable and motivated. Join them on their journey!`,
-          // socialImageLink: "<image-url>",
-        },
+  const payload = {
+    dynamicLinkInfo: {
+      domainUriPrefix: "https://dogether.page.link",
+      link: `https://dogether-78b6f.web.app/user/${auth.currentUser.uid}`,
+      androidInfo: {
+        androidPackageName: "gg.dogether.app",
       },
-    };
+      socialMetaTagInfo: {
+        socialTitle: `New invite from ${auth.currentUser.displayName}`,
+        socialDescription: `${auth.currentUser.displayName} is using Dogether to stay accountable and motivated. Join them on their journey!`,
+        // socialImageLink: "<image-url>",
+      },
+    },
+  };
 
-    const url = `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${WEB_API_KEY}`;
-    const response = await fetch(url, {
+  const url = `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${WEB_API_KEY}`;
+  const [error, response] = await to(
+    fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(payload),
-    });
-    const json = await response.json();
-    await Clipboard.setStringAsync(json.shortLink);
-    alert("Copied to Clipboard!");
-  } catch (error) {
+    })
+  );
+  if (error) {
     alert(error.message);
+    return;
   }
+  const json = await response.json();
+  await Clipboard.setStringAsync(json.shortLink);
+  alert("Copied to Clipboard!");
 };
+
 const Partner = () => {
+  const [hasPartner, setHasPartner] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const unsubHasPartner = onSnapshot(
+      doc(db, `users/${auth.currentUser.uid}`),
+      (doc) => {
+        const { partner } = doc.data();
+        setHasPartner(!!partner);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    const unsubPartnerTodos = onSnapshot(
+      query(
+        collection(db, `users/${auth.currentUser.uid}/todos`),
+        orderBy("createdAt", "desc")
+      ),
+      (querySnapshot) => {
+        const todos = [];
+        querySnapshot.forEach((doc) => {
+          const { title, status } = doc.data();
+          todos.push({
+            id: doc.id,
+            title,
+            status,
+          });
+        });
+        setTodos(todos);
+        console.log("todos: ", todos, "todos.length: ", todos.length);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return () => {
+      unsubHasPartner();
+      unsubPartnerTodos();
+    };
+  }, []);
+
+  const showImage = (item) => {
+    alert("Show image");
+  };
+
+  if (hasPartner) {
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          data={todos}
+          numColumns={1}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.container}>
+              <Ionicons
+                name={
+                  {
+                    unfinished: "md-square-outline",
+                    finished: "md-alert-circle-outline",
+                    verified: "md-checkmark-circle-outline",
+                  }[item.status]
+                }
+                size={24}
+                color="black"
+                style={styles.todoIcon}
+              />
+              <Pressable
+                style={styles.innerContainer}
+                onPress={() => {
+                  if (item.status === "finished") {
+                    showImage(item);
+                  }
+                }}
+              >
+                <Text style={styles.itemHeading}>
+                  {item.title.charAt(0).toUpperCase() + item.title.slice(1)}
+                </Text>
+              </Pressable>
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.message}>Oops! You don't have a partner yet!</Text>
@@ -50,6 +157,7 @@ const Partner = () => {
 };
 
 export default Partner;
+
 const styles = StyleSheet.create({
   message: {
     fontWeight: "bold",
