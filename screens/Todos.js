486393688bc0fbca8c19ef253d579f28b6f6b7c4
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-12-29 02:37:53 -0800

    Fix occasional random crashes when uploading large images
    - see https://github.com/firebase/firebase-js-sdk/issues/5848#issuecomment-1179478627

--- screens/Todos.js
+++ screens/Todos.js
@@ -138,18 +138,9 @@ const Todo = () => {
   };
 
   const getBlobFromUri = async (uri) => {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        resolve(xhr.response);
-      };
-      xhr.onerror = function () {
-        reject(new TypeError("Network request failed"));
-      };
-      xhr.responseType = "blob";
-      xhr.open("GET", uri, true);
-      xhr.send(null);
-    });
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    return blob;
   };
 
   const setTaskProof = async (item, url) => {
